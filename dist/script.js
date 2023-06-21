560b0a55c00a23df32e0c95ad4c0f4e09638e3ee
Felipe Sartorato <felipesartoratoalfano@gmail.com>
2023-06-21 01:15:31 -0300

    Responsividade
    Adicionado a responsividade no projeto

--- dist/script.js
+++ dist/script.js
@@ -7,14 +7,14 @@ let button = document.querySelector(".projects-button");
 let divOcult = document.querySelectorAll(".ocult");
 button.addEventListener("click", () => {
     divOcult.forEach((div) => {
-        const computedStyle = getComputedStyle(div); // Obtém o estilo computado do elemento
-        if (computedStyle.display === "none") { // Verifica se o estilo computado é "none"
-            div.style.display = "block"; // Define o estilo para "block"
-            button.innerHTML = "Mostrar Menos"; // Atualiza o texto do botão para "Mostrar Menos"
+        const computedStyle = getComputedStyle(div);
+        if (computedStyle.display === "none") {
+            div.style.display = "block";
+            button.innerHTML = "Mostrar Menos";
         }
         else {
-            div.style.display = "none"; // Define o estilo para "none"
-            button.innerHTML = "Mostrar Mais"; // Atualiza o texto do botão para "Mostrar Mais"
+            div.style.display = "none";
+            button.innerHTML = "Mostrar Mais";
         }
     });
 });
